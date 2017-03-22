54dc99aae2e7b899a3ff0be81fa069abbc39317d
Thomas Flanitzer <thomas.flanitzer@zuehlke.com>
2017-03-22 15:46:16 +0100

    Eliminated + module convention.

---
+++ cognitive-showcase/src/app/text-analysis/text-analysis.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'text-analysis', component: IntroducerComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(appRoutes)
+  ],
+  declarations: []
+})
+export class TextAnalysisModule { }
